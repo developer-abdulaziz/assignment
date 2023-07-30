a58db881c1e04f52f7b806f38b28ab5ff860dd3b
developer-abdulaziz <abdulazizwebdbdeveloper1996@gmail.com>
2023-07-30 22:41:57 +0600

    renew

--- 01.modiul01_assignment.js
+++ 01.modiul01_assignment.js
@@ -1,50 +1,41 @@
 // 1) Topic: Online Store Discount Calculator (If-Else Statement)
-function discount(purchase) {
-    let discount = 0;
-    
-    if (purchase >= 50 && purchase <= 100) {
-      discount = 5;
+function calculateDiscountedAmount(totalPurchaseAmount) {
+  let discountPercentage = 0;
+  let discountedAmount = 0;
 
-    } 
-    else if (purchase > 100 && purchase <= 200) {
-      discount = 10;
-
-    } 
-    else if (purchase > 200) {
-      discount = 15;
-    }
-  
-    const discountAmount = (discount / 100) * purchase;
-    const discountedTotal = purchase - discountAmount;
-  
-    console.log(discountedTotal.toFixed(2));
-    console.log(discountAmount.toFixed(2));
-    
-
-    console.log(purchase.toFixed(2));
-    console.log(discount + '%');
+  if (totalPurchaseAmount >= 50 && totalPurchaseAmount <= 100) {
+      discountPercentage = 5;
+  } else if (totalPurchaseAmount > 100 && totalPurchaseAmount <= 200) {
+      discountPercentage = 10;
+  } else if (totalPurchaseAmount > 200) {
+      discountPercentage = 15;
   }
-  
 
-  
-  discount(200);
+  discountedAmount = (totalPurchaseAmount * discountPercentage) / 100;
+  let totalAmountAfterDiscount = totalPurchaseAmount - discountedAmount;
 
+  console.log(`Total Purchase Amount: $${totalPurchaseAmount}`);
+  console.log(`Discounted Amount: $${discountedAmount}`);
+  console.log(`Total Amount after Discount: $${totalAmountAfterDiscount}`);
 
+}
+
+calculateDiscountedAmount(200);
 
 
 // 2) Topic: Filter Even Numbers
 function getEvenNumbersOfAnArray(numbers) {
-    const EvenNumbers = [];
-    for (i = 0; i < numbers.length; i++) {
-        let index = i;
-        let element = numbers[index];
-        if (element % 2 === 0) {
-            // console.log(index, element);
-            EvenNumbers.push(element);
-        }
-        // console.log(index, element);
-    }
-    return EvenNumbers;
+  const EvenNumbers = [];
+  for (i = 0; i < numbers.length; i++) {
+      let index = i;
+      let element = numbers[index];
+      if (element % 2 === 0) {
+          // console.log(index, element);
+          EvenNumbers.push(element);
+      }
+      // console.log(index, element);
+  }
+  return EvenNumbers;
 }
 
 var myNumbers =  [12, 34, 45, 23, 6, 78, 54, 90];
@@ -55,46 +46,35 @@ console.log(EvenNumbers);
 
 // 3) Topic: Multiplication Table Generator
 function myfunction(number) {
-    
   
-    for (let i = 1; i <= 10; i++) {
-      const result = i * number;
-      console.log(`${i} x ${number} = ${result}`);
-    }
+
+  for (let i = 1; i <= 10; i++) {
+    const result = i * number;
+    console.log(`${i} x ${number} = ${result}`);
   }
-  
-  // Example usage:
-  myfunction(3);
+}
+
+// Example usage:
+myfunction(3);
 
 // 4) Topic: Grade Calculator (JavaScript Switch Case)
 function calculateGrade(score) {
-    switch (true) {
-      case score >= 90:
-        return 'A';
-      case score >= 80 && score <= 89:
-        return 'B';
-      case score >= 70 && score <= 79:
-        return 'C';
-      case score >= 60 && score <= 69:
-        return 'D';
-      default:
-        return 'F';
-    }
+  switch (true) {
+    case score >= 90:
+      return 'A';
+    case score >= 80 && score <= 89:
+      return 'B';
+    case score >= 70 && score <= 79:
+      return 'C';
+    case score >= 60 && score <= 69:
+      return 'D';
+    default:
+      return 'F';
   }
-  
+}
 
-  const input = 85;
-  const grade = calculateGrade(input);
-  console.log( grade);
 
-  function myfunction(number) {
-    
-  
-    for (let i = 1; i <= 10; i++) {
-      const result = i * number;
-      console.log(`${i} x ${number} = ${result}`);
-    }
-  }
-  
-  // Example usage:
-  myfunction(5);
+const input = 85;
+const grade = calculateGrade(input);
+console.log( grade);
+
